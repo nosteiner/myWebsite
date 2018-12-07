f612bcb08d3ab592c67b5230e51e34fdf4e6718d
Noam Steiner <no.steiner@gmail.com>
2018-12-07 09:27:13 +0200

    fix carousel position

--- src/app/Services/projects.service.ts
+++ src/app/Services/projects.service.ts
@@ -11,6 +11,6 @@ export class ProjectsService {
   constructor(private http: HttpClient) { }
 
   getProjects() {
-  return this.http.get<Project[]>('/projects')
+  return this.http.get<Project[]>('/projects');
   }
 }
